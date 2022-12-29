9fa3c214bdb57c0e8a3daa59e6bb3f9f2d80168c
SyedAbdullawaseem <116788158+SyedAbdullawaseem@users.noreply.github.com>
2022-12-29 17:11:37 +0530

    LocalStorage

--- src/Store/auth-context.js
+++ src/Store/auth-context.js
@@ -9,16 +9,19 @@ const AuthContext=React.createContext({
 
 export const AuthContextProvider=props=>{
 
-    const  [token, setToken]= useState(null)
+    const initialToken=localStorage.getItem('token')
+    const  [token, setToken]= useState(initialToken)
 
     const userIsLoggedIn=!!token;
 
     const loginHandler=(token)=>{
+        localStorage.setItem('token', token)
         setToken(token)
     }
 
     const logOutHandler=()=>{
         setToken(null)
+        localStorage.removeItem('token')
     }
 
     const contextValue={
